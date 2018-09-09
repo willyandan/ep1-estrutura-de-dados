dc24094bc926b9e958fc3641218a7cb7a3386114
willyan antunes - MAC <willyan.dantunes@gmail.com>
2018-09-09 16:47:48 -0300

    merge sort pronto

---
+++ main.py
@@ -0,0 +1,8 @@
+import random, mergesort
+def main():
+    print("EP 1 - Estrutura de dados")
+    vet = list(range(21))
+    random.shuffle(vet)
+    
+if __name__ == '__main__':
+    main()
---
+++ mergesort.py
@@ -0,0 +1,14 @@
+def mergeSort(vet):
+    if(len(vet) < 2): return vet
+    meio = len(vet)//2 
+    return merge(rlist=mergeSort(vet[meio:]), llist=mergeSort(vet[:meio]))
+def merge(rlist =[], llist = []):
+    final = []
+    while rlist and llist:
+        if llist[0] < rlist[0]:
+            final.append(llist.pop(0))
+        else:
+            final.append(rlist.pop(0))
+    if(llist): return final + llist
+    if(rlist): return final + rlist
+    return final
